07282d7e84e3b70d0f83b579abdb7dfc6e055247
shakaylaA <salsto26@uncc.edu>
2021-03-01 21:41:51 -0500

    Finish part 2

--- NameFrequency.py
+++ NameFrequency.py
@@ -17,12 +17,13 @@ class NameFrequency:
     # making data frame
     def preparingData(self, file_name, column_name):
         data = pd.read_csv(file_name)
-        data.dropna(inplace=True)
+        data.dropna(inplace=False)
         data_frame = dict(data[column_name].str.split(" ", n=1, expand=True))
         self.data_frame = data_frame
         if isinstance(self.data_frame, pd.DataFrame):
-            return True
-        return False
+            return False
+        return True
+
 
     # making separate data set column from specific data frame
     def selectingColumn(self, column_name, column_index):
